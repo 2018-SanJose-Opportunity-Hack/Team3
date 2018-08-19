4ae3f8ce6d6a3d085f2e577be9c4aaf7abd9285b
suraj khurana <suraj.r.khurana@gmail.com>
2018-08-19 09:47:35 -0700

    scheduler to send emails notificiaton
    reservation

--- routes/api/reservation.js
+++ routes/api/reservation.js
@@ -12,6 +12,7 @@ const TimeBlock = mongoose.model('timeblocks');
 const Reservation  = mongoose.model('reservations');
 const subscribed = mongoose.model('subscribe');
 const schedule = require('node-schedule');
+const sendEmail  = require('client/src/util/notification');
 
 
 
@@ -387,6 +388,56 @@ let openTime = new Date();
         }
 
         })
+
+});
+
+//Job For sending checkin emails
+let k = schedule.scheduleJob('45 * * * * ', function(){
+
+
+    // let openTime =new Date();
+    let openTime = new Date();
+
+//for 45 min-th query,add +1 to hour.
+    openTime.setHours(openTime.getHours()+1)
+
+
+
+    // let roundTime= roundMinutes(openTime)
+
+    console.log("Scheduler ran at :" +openTime )
+
+    Reservation.find({
+        startTime:
+            {
+                $lt:openTime
+            },
+        endTime:
+            {
+                $gte: openTime
+            },
+        status:"Upcoming"
+
+    }).then(retReservation=>{
+
+        for ( let i = 0; i < retReservation.length; i++) {
+            let  CurrReserveUser = retReservation[i].user
+
+
+            User.findById(
+                CurrReserveUser
+            ).then(currUser=>{
+                var emailText = 'hi there, Please check In - From San Jose Sports Facility Reservation ';
+                //TODO : hardcoding email as it
+                var toEmail = currUser.email;
+                sendEmail(emailText,toEmail);
+
+            })
+
+
+        }
+
+    })
 
 });
 
